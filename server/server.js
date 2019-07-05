e843da0deb05a40af67dbd64b4ccead0b149c7fa
soroush <ahmadisorosh1@gmail.com>
2019-07-06 01:22:02 +0430

    add GET: /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb');
+
 
 
 const app = express();
@@ -34,6 +36,28 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e)
     })
+});
+
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+             return res.status(404).send();
+        }
+
+        res.send({todo})
+        
+    }).catch((e) => {
+        res.status(404).send();
+    })
+
+
 });
 
 
